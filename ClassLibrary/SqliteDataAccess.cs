6c24f21b146a82c508ebe15bdd7649263dcfca8a
lforche <55415491+lforche@users.noreply.github.com>
2021-03-29 12:17:56 -0400

    GUI for Customer Home Page Added
    Main thing done was the GUI for the Customer Home Page was added, so everyone has access. The database is also within the application. A few nuget packages were added (like dapper and sqlite). I am hoping that it adds to GitHub correctly, but I will check that. No functionality was added yet. There is a separate class library where we can put the actual classes and functionality of the classes, so it is separate from the UI if we want. That is how the guy in the youtube video did it.

---
+++ ClassLibrary/SqliteDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Air3550
+{
+    public class SqliteDataAccess
+    {
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
