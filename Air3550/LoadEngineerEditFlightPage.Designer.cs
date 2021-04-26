239faed08a746d0d87f57fe80e8e9a268572a595
Thomas Runyan <thomasrunyan13@gmail.com>
2021-04-26 18:02:38 -0400

    Bug Fix: Edit Flight Save Button not working

--- Air3550/LoadEngineerEditFlightPage.Designer.cs
+++ Air3550/LoadEngineerEditFlightPage.Designer.cs
@@ -32,53 +32,52 @@ namespace Air3550
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadEngineerEditFlightPage));
             this.routeTimePicker = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            this.saveFlight = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // routeTimePicker
             // 
-            this.routeTimePicker.Font = new System.Drawing.Font("Rockwell", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.routeTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.routeTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.routeTimePicker.Location = new System.Drawing.Point(146, 226);
-            this.routeTimePicker.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.routeTimePicker.Location = new System.Drawing.Point(73, 118);
             this.routeTimePicker.Name = "routeTimePicker";
-            this.routeTimePicker.Size = new System.Drawing.Size(295, 58);
+            this.routeTimePicker.Size = new System.Drawing.Size(150, 32);
             this.routeTimePicker.TabIndex = 17;
             this.routeTimePicker.ValueChanged += new System.EventHandler(this.routeTimePicker_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Rockwell", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(48, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(481, 161);
+            this.label1.Size = new System.Drawing.Size(238, 91);
             this.label1.TabIndex = 19;
             this.label1.Text = resources.GetString("label1.Text");
             // 
-            // saveFlight
+            // saveButton
             // 
-            this.saveFlight.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.saveFlight.Font = new System.Drawing.Font("Rockwell", 10F);
-            this.saveFlight.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.saveFlight.Location = new System.Drawing.Point(142, 293);
-            this.saveFlight.Name = "saveFlight";
-            this.saveFlight.Size = new System.Drawing.Size(299, 72);
-            this.saveFlight.TabIndex = 38;
-            this.saveFlight.Text = "Save Changes";
-            this.saveFlight.UseVisualStyleBackColor = false;
+            this.saveButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.saveButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.saveButton.Location = new System.Drawing.Point(71, 152);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(150, 37);
+            this.saveButton.TabIndex = 38;
+            this.saveButton.Text = "Save Changes";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // LoadEngineerEditFlightPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(586, 454);
-            this.Controls.Add(this.saveFlight);
+            this.ClientSize = new System.Drawing.Size(293, 236);
+            this.Controls.Add(this.saveButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.routeTimePicker);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoadEngineerEditFlightPage";
@@ -92,6 +91,6 @@ namespace Air3550
         #endregion
         private System.Windows.Forms.DateTimePicker routeTimePicker;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button saveFlight;
+        private System.Windows.Forms.Button saveButton;
     }
 }
